80a1a1046c8eb4a46e0430d5400f7d33aa88a005
Rajieshn <rajieshn@in-rajieshn.local>
2017-03-29 17:37:02 +0530

    Pass the release and base arch names as params

--- initialize.sh
+++ initialize.sh
@@ -1,20 +1,23 @@
 set -e
 
+RELEASE_SERVER = $1
+BASE_ARCH = $2
+
 /bin/cat << EOF > /etc/yum.repos.d/base.repo
 [base]
-name=CentOS-$releasever - Base
-baseurl=http://fmtsd01.thoughtworks.com/centos/6.8/os/$basearch/
+name=CentOS-$RELEASE_SERVER - Base
+baseurl=http://fmtsd01.thoughtworks.com/centos/6.8/os/$BASE_ARCH/
 enabled=1
 fastestmirror_enabled=0
 gpgcheck=0
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
-mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os
+mirrorlist=http://mirrorlist.centos.org/?release=$RELEASE_SERVER&arch=$BASE_ARCH&repo=os
 EOF
 
 /bin/cat << EOF > /etc/yum.repos.d/centos-release-scl-rh.repo
 [centos-release-scl-rh]
 name=Yum Repository
-baseurl=http://mirror.centos.org/centos/$releasever/sclo/$basearch/rh/
+baseurl=http://mirror.centos.org/centos/$RELEASE_SERVER/sclo/$BASE_ARCH/rh/
 enabled=1
 fastestmirror_enabled=0
 gpgcheck=1
